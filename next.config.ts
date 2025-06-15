6e141be052c551f314658a517eea8c22b00e2438
sergei.zhezniakowski <sergei.zhezniakowski@nytimes.com>
2025-06-14 23:33:58 -0400

    SCAFFOLDING: next js output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    output: 'export',
+    basePath: '/question-bank',
 };
 
 export default nextConfig;
